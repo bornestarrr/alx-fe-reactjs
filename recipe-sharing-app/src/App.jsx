44d0e2f433e2279424b70a07929e332b715c98c6
bornestarrr <kamoseleka0@gmail.com>
2025-07-23 15:25:45 +0200

    Add recipe detail view, editing, deleting with routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,35 +1,18 @@
+// App.jsx
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
-function App() {
+const App = () => {
   return (
-    <div style={{ maxWidth: '600px', margin: 'auto', padding: '20px' }}>
-      <h1>Recipe Sharing App</h1>
+    <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
-    </div>
+    </Router>
   );
-}
-
-// Wrapper to get recipeId from URL params and pass to RecipeDetails
-import { useParams } from 'react-router-dom';
-const RecipeDetailsWrapper = () => {
-  const { id } = useParams();
-  const recipeId = Number(id);
-  return <RecipeDetails recipeId={recipeId} />;
 };
 
 export default App;
