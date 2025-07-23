8eaeb6d00e0a7046db5bdae94739b64cb0202fc2
bornestarrr <kamoseleka0@gmail.com>
2025-07-24 00:53:03 +0200

    Add recipe detail view, editing, deleting with routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import AddRecipeForm from './components/AddRecipeForm';
+import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
-import RecipeForm from './components/RecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
-import SearchBar from './components/SearchBar';
 
-function App() {
+import { useRecipeStore } from './components/recipeStore';
+
+const App = () => {
+  const searchTerm = useRecipeStore(state => state.searchTerm);
+  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+
+  useEffect(() => {
+    filterRecipes();
+  }, [searchTerm, filterRecipes]);
+
   return (
     <Router>
-      <div>
+      <div style={{ padding: '2rem' }}>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <AddRecipeForm />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <h1>Recipe Sharing App</h1>
-                <SearchBar />
-                <RecipeList />
-                <RecipeForm />
-              </div>
-            }
-          />
+          <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/edit/:id" element={<EditRecipeForm />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
