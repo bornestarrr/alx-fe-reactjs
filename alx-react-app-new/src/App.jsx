822383ddaa8da2112cf8177ab5541ed8c939f2ed
bornestarrr <kamoseleka0@gmail.com>
2025-07-25 13:32:03 +0200

    add new

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </>
+  );
+}
+
+export default App;
