fde13c5382cb765f1ca95c93f74c501138fa327e
bornestarrr <kamoseleka0@gmail.com>
2025-07-27 23:47:15 +0200

    refactor app

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
-// src/components/UserProfile.jsx
+import { useContext } from 'react';
+import { UserContext } from '../UserContext';
 
-function UserProfile(props) {
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue', fontSize: '20px' }}>{props.name}</h2>
-      <p>
-        Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
-      </p>
-      <p>Bio: {props.bio}</p>
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
