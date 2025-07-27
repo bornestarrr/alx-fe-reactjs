8ad71ef32c9865f9b11afc33463cb8fa9e0a95c2
bornestarrr <kamoseleka0@gmail.com>
2025-07-27 23:35:24 +0200

    refactor app

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,14 @@
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+function App() {
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
