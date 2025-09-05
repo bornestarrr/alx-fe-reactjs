8c0f2cca8dd017266c6b8fbc3c15be528a0e468c
bornestarrr <kamoseleka0@gmail.com>
2025-09-06 00:32:48 +0200

    Rename FormikForm.jsx to formikForm.js for checker compatibility

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,11 @@
 import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
-    <div className="p-8">
+    <div className="p-8 space-y-12">
       <RegistrationForm />
+      <FormikForm />
     </div>
   );
 }
