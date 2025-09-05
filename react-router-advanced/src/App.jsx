000759e773c8b72885ae4b42c75d0613135fcc98
bornestarrr <kamoseleka0@gmail.com>
2025-09-06 01:02:54 +0200

    Fix nested git repo and add form-handling-react project

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
 import User from "./components/User";
+import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-const isAuthenticated = true; // change to false to test redirect
+const isAuthenticated = true;
 
 function App() {
   return (
@@ -21,11 +20,10 @@ function App() {
               <Profile />
             </ProtectedRoute>
           }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+        />
         <Route path="/user/:userId" element={<User />} />
+        {/* New dynamic blog route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function BlogPost() {
+  const { id } = useParams();
+  return <p>Viewing blog post with ID: {id}</p>;
+}
